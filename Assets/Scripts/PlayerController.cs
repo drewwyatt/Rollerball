d9aa3d61359b9420037f389aab96d45f8adac145
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-02-16 16:27:53 -0500

    moving the player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    
+    public void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnMove(InputValue value) {
+        var vec = value.Get<Vector2>();
+        movementX = vec.x;
+        movementY = vec.y;
+    }
+
+    public void FixedUpdate() {
+        var movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+}
