098fb51590e7c8d565df2037198d4e75d6327816
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-02-16 21:44:39 -0500

    add pickups

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+    public void Update() {
+      transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
